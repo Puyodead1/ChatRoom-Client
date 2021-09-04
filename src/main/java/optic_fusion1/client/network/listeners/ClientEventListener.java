ea14f8da799525990be6843f9ba537a0db406c46
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2021-09-03 22:54:45 -0400

    Init

---
+++ src/main/java/optic_fusion1/client/network/listeners/ClientEventListener.java
@@ -0,0 +1,24 @@
+package optic_fusion1.client.network.listeners;
+
+import optic_fusion1.client.network.SocketClient;
+import optic_fusion1.packets.IPacket;
+
+import java.net.Socket;
+
+public interface ClientEventListener {
+
+    default void onPreConnect() {
+    }
+
+    default void onConnectionEstablished() {
+    }
+
+    default void onDisconnect() {
+    }
+
+    default void onRawPacketReceive(final byte[] packet) {
+    }
+
+    default void onPacketReceive(final SocketClient socketClient, final IPacket packet) {
+    }
+}
