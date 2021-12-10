a3430f8dbfa727b39aae75127dc1ee57bd1e91e8
Puyodead1 <14828766+Puyodead1@users.noreply.github.com>
2021-12-10 13:21:34 -0500

    log4j and joptsimple + minor changes
    - Removed TerminalConsoleAppender from log4j config
    + Disabled file logger for development purposes
    + Added Log4J
    + Added JOptSimple
    + Git submodules

--- src/main/java/optic_fusion1/client/ChatRoomClient.java
+++ src/main/java/optic_fusion1/client/ChatRoomClient.java
@@ -1,23 +1,55 @@
 package optic_fusion1.client;
 
+import com.sun.tools.javac.Main;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.fusesource.jansi.AnsiConsole;
 
 public class ChatRoomClient {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        AnsiConsole.systemInstall();
+
+        OptionParser optionParser = new OptionParser();
+        OptionSpec<Void> helpSpec = optionParser.accepts("help").forHelp();
+        OptionSpec<String> hostSpec = optionParser.accepts("host", "Server host address").withOptionalArg().defaultsTo("localhost");
+        OptionSpec<Integer> portSpec = optionParser.accepts("port", "Server port").withOptionalArg().ofType(Integer.class).defaultsTo(8888);
+        OptionSpec<String> usernameSpec = optionParser.accepts("username", "Server account username").withOptionalArg();
+        OptionSpec<String> passwordSpec = optionParser.accepts("password", "Server account password").withOptionalArg();
+
         EventLoopGroup group = new NioEventLoopGroup();
+
         try {
+            OptionSet optionSet = optionParser.parse(args);
+
+            if (optionSet.has(helpSpec)) {
+                optionParser.printHelpOn(System.out);
+                return;
+            }
+
+            String serverHost = optionSet.valueOf(hostSpec);
+            int serverPort = optionSet.valueOf(portSpec);
+            String username = optionSet.valueOf(usernameSpec);
+            String password = optionSet.valueOf(passwordSpec);
+
             Bootstrap b = new Bootstrap();
             b.group(group)
                     .channel(NioSocketChannel.class)
                     .handler(new ClientChannelInitializer());
 
-            b.connect("localhost", 8888).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            b.connect(serverHost, serverPort).sync().channel();
+
+        } catch (Exception e) {
+            LOGGER.fatal(String.format("Failed to start ChatRoom client: %s", e.getLocalizedMessage()));
         } finally {
             group.shutdownGracefully();
         }
